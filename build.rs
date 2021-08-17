ffaa48e6b3d7679a1592cea655fb60ec56d125c7
Brandon Weeks <bweeks@google.com>
2021-08-17 13:51:58 -0700

    Infer correct type for CKK_* values
    Change-Id: I23f0dada77aaa772997aa159229ed43610cddd68

--- build.rs
+++ build.rs
@@ -8,17 +8,18 @@ pub struct CargoCallbacks;
 impl callbacks::ParseCallbacks for CargoCallbacks {
     // https://github.com/rust-lang/rust-bindgen/issues/1594
     fn int_macro(&self, name: &str, _value: i64) -> Option<callbacks::IntKind> {
-        if name.starts_with("CK_")
+        if ["CK_TRUE", "CK_FALSE"].contains(&name) {
+            Some(bindgen::callbacks::IntKind::U8)
+        } else if name.starts_with("CK_")
             || name.starts_with("CKA_")
             || name.starts_with("CKF_")
+            || name.starts_with("CKK_")
             || name.starts_with("CKM_")
-            || name.starts_with("CKR_")
             || name.starts_with("CKO_")
+            || name.starts_with("CKR_")
             || name.starts_with("CKS_")
         {
             Some(bindgen::callbacks::IntKind::U64)
-        } else if ["CK_TRUE", "CK_FALSE"].contains(&name) {
-            Some(bindgen::callbacks::IntKind::U8)
         } else {
             None
         }
@@ -30,7 +31,7 @@ impl callbacks::ParseCallbacks for CargoCallbacks {
 }
 
 fn main() {
-    println!("cargo:rerun-if-changed=wrapper.h");
+    println!("cargo:rerun-if-changed=third_party/pkcs11unix.h");
 
     let bindings = bindgen::Builder::default()
         .header("third_party/pkcs11unix.h")
